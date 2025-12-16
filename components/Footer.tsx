889d9e2858137fd5b29843865b6d568229ae8104
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-12-16 20:25:14 +0530

    update footer insta link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -56,7 +56,7 @@ const Footer: React.FC = () => {
               <a href="#" className="w-10 h-10 bg-white/5 flex items-center justify-center hover:bg-gold-600 transition-colors">
                 <Youtube size={18} />
               </a>
-              <a href="#" className="w-10 h-10 bg-white/5 flex items-center justify-center hover:bg-gold-600 transition-colors">
+              <a href="https://www.instagram.com/ironcrestdevelopers?igsh=OWhvdXd6anJzMXhs" target="_blank" className="w-10 h-10 bg-white/5 flex items-center justify-center hover:bg-gold-600 transition-colors">
                 <Instagram size={18} />
               </a>
               <a href="http://linkedin.ironcrestdevelopers.com/" target="_blank" className="w-10 h-10 bg-white/5 flex items-center justify-center hover:bg-gold-600 transition-colors">
